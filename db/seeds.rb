cf1e742a81467e040413dc1d21248230d8fe9eda
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-01-06 11:55:49 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,40 @@ require "./app"
 
 Dir[File.expand_path "#{Rack::Directory.new("").root}/spec/factories/*.rb"].sort.each { |file| require file }
 
+def create_user num
+  user = FactoryBot.create(:user, id: num)
+  "User created!" if user
+end
 
-user = FactoryBot.create(:user)
-puts "User created!" if user
-50000.times do |i|
-  article_category = FactoryBot.create(:article_category, id: i + 1)
-  puts "#{i + 1}: Article Category Created!\n #{article_category.to_json}" if article_category
+def create_article_category num
+  article_category = FactoryBot.create(:article_category, id: num + 1)
+  "#{num + 1}: Article Category Created!\n #{article_category.to_json}" if article_category
+end
+
+def create_article user_id, article_category_id, num
+  article = FactoryBot.create(:article, user_id: user_id, article_category_id: article_category_id, id: num + 1)
+  "#{num + 1}: Article Created!\n #{article.to_json}" if article
+end
+
+50.times do |i|
+  puts create_user(i + 1)
 rescue
   retry
 end
 
+50.times do |i|
+  puts create_article_category(i + 1)
+rescue
+  retry
+end
+users = User.all
+article_categories = ArticleCategory.all
 
-# x = 1
-# 50.times do |i|
-#   case i
-#     puts "#{i + 1}: Article Created!" if FactoryBot.create(:article, user_id: user.id, article_category_id: ArticleCategory.find(x).id)
-#   when 1, 10, 20, 30, 40, 50
-#     puts "#{i + 1}: Article Created!" if FactoryBot.create(:article, user_id: user.id, article_category_id: ArticleCategory.find(x).id)
-#     x += 1
-#   end
-# end
+500.times do |i|
+  user_id = users.find(rand(51).to_i + 1).id
+  article_category_id = article_categories.find(rand(51).to_i + 1).id
+  puts create_article(user_id, article_category_id, i)
+rescue
+  retry
+end
+
