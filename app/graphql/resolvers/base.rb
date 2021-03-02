9a0b60f6ce2d32322a9713ef1cd2431050d839b3
POPPIN-FUMI <fumitake.kawasaki@el-soul.com>
2021-03-02 11:14:20 +0100

    add base resolver

--- app/graphql/resolvers/base.rb
+++ app/graphql/resolvers/base.rb
@@ -1,5 +1,9 @@
 module Resolvers
   class Base < GraphQL::Schema::Resolver
     # argument_class Arguments::Base
+    def decode_global_key(id)
+      _, data_id = SoulsApiSchema.from_global_id id
+      data_id
+    end
   end
 end
